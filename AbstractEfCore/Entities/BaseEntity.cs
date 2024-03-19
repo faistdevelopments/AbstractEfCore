fb09c8848049a72201acc34c1f68396f6dd5297a
Ben Faist <36361398+bfa-faistdev@users.noreply.github.com>
2024-03-19 19:48:47 +0100

    Add basic project (#4)
    Co-authored-by: Ben Faist <bfaist>

---
+++ AbstractEfCore/Entities/BaseEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace com.faistdevelopments.AbstractEfCore;
+
+public abstract class BaseEntity<T> : AbstractEntity where T : AbstractEntity
+{
+    public void Save(Database db)
+    {
+        db.Add<T>((this as T)!);
+    }
+
+    private static DbSet<T> GetSet(Database db)
+    {
+        return db.GetSet<T>(typeof(T));
+    }
+
+    public static List<T> FetchAll(Database db)
+    {
+        List<T> baseEntities = GetSet(db).ToList();
+        return baseEntities!;
+    }
+
+    public static T? FetchByPrimaryKey(Database db, params object[] pks)
+    {
+        DbSet<T> set = GetSet(db);
+        AbstractEntity? baseEntity = set.Find(pks);
+
+        if (baseEntity == null)
+        {
+            return null;
+        }
+
+        return baseEntity as T;
+    }
+}
+
